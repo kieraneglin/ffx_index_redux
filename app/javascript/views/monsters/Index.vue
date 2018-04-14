aa3f1c55dd15a62e32642702b6f1d6531f49ce51
Kieran Eglin <kieran.eglin@gmail.com>
2018-04-14 11:49:34 -0700

    added babel configs for async/await

--- app/javascript/views/monsters/Index.vue
+++ app/javascript/views/monsters/Index.vue
@@ -2,7 +2,13 @@
   h1 hey
 </template>
 <script>
+import axios from 'axios'
+
 export default {
-  
+  async created() {
+    let response = await axios.get('/api/v1/monsters')
+
+    console.log(response)
+  }  
 }
 </script>
