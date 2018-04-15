fc9096073ca1e17b6402e5d8f03037bb32f20cd0
Kieran Eglin <kieran.eglin@gmail.com>
2018-04-15 10:54:44 -0700

    added item show in Vue

--- app/javascript/views/items/Show.vue
+++ app/javascript/views/items/Show.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-  h1 item show
+  code {{ item }}
 </template>
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('item', {
+      item: state => state.item
+    })
+  },
+  async created() {
+    await this.$store.dispatch('item/fetch', this.$route.params.slug)
+  },
+  beforeDestroy() {
+    this.$store.commit('item/clear')
+  }
+}
+</script>
